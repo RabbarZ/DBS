2a0633f0c9677ea3f5b627617ec2c93d3f083f46
Kilian Hürlimann <kilian.huerlimann@outlook.com>
2023-04-03 17:57:25 +0200

    added models to serialize json data from IGDB

---
+++ EngineTool/EngineTool/JsonModels/Website.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EngineTool.JsonModels
+{
+    public class Website
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("category")]
+        public int Category { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+}
