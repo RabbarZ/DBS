21a2acab4856291b64de34c98ad95a67aa269233
Kilian Hürlimann <kilian.huerlimann@outlook.com>
2024-05-03 16:09:13 +0200

    adapted new using() method

--- EngineTool/EngineTool/Services/DbService.cs
+++ EngineTool/EngineTool/Services/DbService.cs
@@ -7,34 +7,26 @@ namespace EngineTool.Services
     {
         public static void EnsureDbExists()
         {
-            using (var context = new EngineContext())
-            {
-                context.Database.EnsureCreated();
-            }
+            using var context = new EngineContext();
+            context.Database.EnsureCreated();
         }
 
         public static Game GetGameByIdgbId(int igdbId)
         {
-            using (var context = new EngineContext())
-            {
-                return context.Games.AsNoTracking().SingleOrDefault(g => g.IgdbId == igdbId);
-            }
+            using var context = new EngineContext();
+            return context.Games.AsNoTracking().SingleOrDefault(g => g.IgdbId == igdbId);
         }
 
         public static Engine GetEngineByIdgbId(int igdbId)
         {
-            using (var context = new EngineContext())
-            {
-                return context.Engines.AsNoTracking().SingleOrDefault(e => e.IgdbId == igdbId);
-            }
+            using var context = new EngineContext();
+            return context.Engines.AsNoTracking().SingleOrDefault(e => e.IgdbId == igdbId);
         }
 
         public static bool GetEngineContainsGame(Guid engineId, Guid gameId)
         {
-            using (var context = new EngineContext())
-            {
-                return context.Engines.AsNoTracking().Where(e => e.Id == engineId).Any(e => e.Games.Any(g => g.Id == gameId));
-            }
+            using var context = new EngineContext();
+            return context.Engines.AsNoTracking().Where(e => e.Id == engineId).Any(e => e.Games.Any(g => g.Id == gameId));
         }
 
         public static Engine AddEngine(Engine engine)
