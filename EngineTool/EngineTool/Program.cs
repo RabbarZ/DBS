ecce97eb2988b5d3061980a9bb3f00885a187de6
Kilian Hürlimann <kilian.huerlimann@outlook.com>
2024-05-06 21:47:09 +0200

    split code in main() to multiple methods

--- EngineTool/EngineTool/Program.cs
+++ EngineTool/EngineTool/Program.cs
@@ -35,7 +35,7 @@ internal static class Program
                 });
 
                 services.AddTransient<ISteamService, SteamService>();
-                
+
                 services.AddTransient<IRepository<Game>, Repository<Game>>();
                 services.AddTransient<IRepository<Engine>, Repository<Engine>>();
                 services.AddTransient<IRepository<PlayerStats>, Repository<PlayerStats>>();
@@ -60,104 +60,135 @@ internal static class Program
         var igdbService = host.Services.GetRequiredService<IIgdbService>();
         var steamService = host.Services.GetRequiredService<ISteamService>();
 
-        var gameService = host.Services.GetRequiredService<IGameService> ();
+        var gameService = host.Services.GetRequiredService<IGameService>();
         var engineService = host.Services.GetRequiredService<IEngineService>();
         var playerStatsService = host.Services.GetRequiredService<IPlayerStatsService>();
         var ratingService = host.Services.GetRequiredService<IRatingService>();
 
         var timestamp = DateTime.UtcNow;
 
-        var games = igdbService.GetGamesAsync(500);
+        var games = igdbService.GetGamesAsync(1000);
 
-        int i = 1;
         await foreach (var igdbGame in games)
         {
             try
             {
-                int? steamId = igdbService.GetSteamId(igdbGame);
-                if (steamId == null)
-                {
-                    Console.WriteLine("Could not fetch Steam ID from IGDB.");
-                    continue;
-                }
-
-                Console.WriteLine($"{i} : {igdbGame.Name} : {steamId}");
-
-                var playerCount = await steamService.GetCurrentPlayerCountAsync(steamId.Value);
-                if (playerCount == null)
-                {
-                    Console.WriteLine("Could not fetch current player count from Steam.");
-                    continue;
-                }
-
-                var rating = await steamService.GetRatingAsync(steamId.Value);
-                if (rating == null)
-                {
-                    Console.WriteLine("Could not fetch rating from Steam.");
-                    continue;
-                }
-
-                var dbGame = gameService.GetByIgdbId(igdbGame.Id);
-                if (dbGame == null)
-                {
-                    dbGame = new Game
-                    {
-                        Id = Guid.NewGuid(),
-                        Name = igdbGame.Name,
-                        IgdbId = igdbGame.Id,
-                        SteamId = steamId.Value
-                    };
-
-                    gameService.Add(dbGame);
-                }
-
-                playerStatsService.Add(new PlayerStats
-                {
-                    Id = Guid.NewGuid(),
-                    GameId = dbGame.Id,
-                    PlayerCount = playerCount.Value,
-                    Timestamp = timestamp
-                });
-
-                ratingService.Add(new Rating
-                {
-                    Id = Guid.NewGuid(),
-                    GameId = dbGame.Id,
-                    Score = rating.Score,
-                    ScoreDescription = rating.ScoreDescription,
-                    Timestamp = timestamp
-                });
-
-                foreach (var igdbEngine in igdbGame.Engines)
-                {
-                    var dbEngine = engineService.GetByIgdbId(igdbEngine.Id);
-                    if (dbEngine == null)
-                    {
-                        dbEngine = new Engine
-                        {
-                            Id = Guid.NewGuid(),
-                            IgdbId = igdbEngine.Id,
-                            Name = igdbEngine.Name
-                        };
-
-                        engineService.Add(dbEngine);
-                    }
-
-                    var containsGame = engineService.GetContainsGame(dbEngine.Id, dbGame.Id);
-
-                    if (containsGame != null && !containsGame.Value)
-                    {
-                        dbEngine.Games.Add(dbGame);
-                        engineService.Update(dbEngine);
-                    }
-                }
-
-                i++;
+                await ProcessGameAsync(igdbGame, igdbService, steamService, gameService, engineService, playerStatsService, ratingService, timestamp);
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine(ex.ToString());
+                Console.WriteLine(ex.ToString());
+                Console.ReadKey();
             }
+        }
+    }
+
+    private static async Task ProcessGameAsync(
+        IgdbGame igdbGame,
+        IIgdbService igdbService,
+        ISteamService steamService,
+        IGameService gameService,
+        IEngineService engineService,
+        IPlayerStatsService playerStatsService,
+        IRatingService ratingService,
+        DateTime timestamp)
+    {
+        int? steamId = igdbService.GetSteamId(igdbGame);
+        if (steamId == null)
+        {
+            Console.WriteLine("Could not fetch Steam ID from IGDB.");
+            return;
+        }
+
+        var playerCount = await steamService.GetCurrentPlayerCountAsync(steamId.Value);
+        if (playerCount == null)
+        {
+            Console.WriteLine("Could not fetch current player count from Steam.");
+            return;
+        }
+
+        var rating = await steamService.GetRatingAsync(steamId.Value);
+        if (rating == null)
+        {
+            Console.WriteLine("Could not fetch rating from Steam.");
+            return;
+        }
+
+        var dbGame = GetOrCreateGame(igdbGame, steamId.Value, gameService);
+        if (dbGame == null)
+        {
+            return;
+        }
+
+        playerStatsService.Add(new PlayerStats
+        {
+            Id = Guid.NewGuid(),
+            GameId = dbGame.Id,
+            PlayerCount = playerCount.Value,
+            Timestamp = timestamp
+        });
+
+        ratingService.Add(new Rating
+        {
+            Id = Guid.NewGuid(),
+            GameId = dbGame.Id,
+            Score = rating.Score,
+            ScoreDescription = rating.ScoreDescription,
+            Timestamp = timestamp
+        });
+
+        foreach (var igdbEngine in igdbGame.Engines)
+        {
+            ProcessEngine(igdbEngine, dbGame, engineService);
+        }
+    }
+
+    private static Game GetOrCreateGame(
+        IgdbGame igdbGame,
+        int steamId,
+        IGameService gameService)
+    {
+        var dbGame = gameService.GetByIgdbId(igdbGame.Id);
+        if (dbGame == null)
+        {
+            dbGame = new Game
+            {
+                Id = Guid.NewGuid(),
+                Name = igdbGame.Name,
+                IgdbId = igdbGame.Id,
+                SteamId = steamId
+            };
+
+            gameService.Add(dbGame);
+        }
+
+        return dbGame;
+    }
+
+    private static void ProcessEngine(
+        IgdbEngine igdbEngine,
+        Game dbGame,
+        IEngineService engineService)
+    {
+        var dbEngine = engineService.GetByIgdbId(igdbEngine.Id);
+        if (dbEngine == null)
+        {
+            dbEngine = new Engine
+            {
+                Id = Guid.NewGuid(),
+                IgdbId = igdbEngine.Id,
+                Name = igdbEngine.Name
+            };
+
+            engineService.Add(dbEngine);
+        }
+
+        var containsGame = engineService.GetContainsGame(dbEngine.Id, dbGame.Id);
+
+        if (containsGame != null && !containsGame.Value)
+        {
+            dbEngine.Games.Add(dbGame);
+            engineService.Update(dbEngine);
         }
     }
 }
